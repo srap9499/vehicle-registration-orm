2369f71c96159f5cbea30cac2ac7debd560b1a06
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-04-20 11:57:58 +0530

    Project Initialization

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: 'localhost',
+    USER: 'demo',
+    PASSWORD: 'demo123',
+    DB: 'vehicle_registration_db',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
